5e3c3841b6fe3d5a8b8f84104f4deb584009ba66
Rogatien de La Brunetière <rogatien.delabrunetire@gmail.com>
2025-02-04 14:34:00 +0100

    Init test case

--- src/test/java/com/rogue/softway/test/HealthDiagnosisTest.java
+++ src/test/java/com/rogue/softway/test/HealthDiagnosisTest.java
@@ -1,7 +1,13 @@
 package com.rogue.softway.test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,20 +25,45 @@ public class HealthDiagnosisTest {
 
 	@Test
 	public void multipleOf3Test() {
+		final int testValue = 33;
+
+		final List<String> diagnosisResult = healthDiagnosis.redirectPatiens(testValue);
+
+		assertAll("Multiple of 3 test", () -> assertEquals(1, diagnosisResult.size()),
+				() -> assertEquals("traumatologie", diagnosisResult.get(0)));
 	}
 
 	@Test
 	public void multipleOf5Test() {
-		fail("Not yet implemented");
+		final int testValue = 55;
+
+		final List<String> diagnosisResult = healthDiagnosis.redirectPatiens(testValue);
+
+		assertAll("Multiple of 5 test", () -> assertEquals(1, diagnosisResult.size()),
+				() -> assertEquals("cardiologie", diagnosisResult.get(0)));
 	}
 
 	@Test
 	public void multipleOf3and5Test() {
-		fail("Not yet implemented");
+		final int testValue = 15;
+
+		final List<String> resultExpected = Arrays.asList("cardiologie", "traumatologie");
+
+		final List<String> diagnosisResult = healthDiagnosis.redirectPatiens(testValue);
+
+		assertAll("Multiple of 3 and 5 test", () -> assertEquals(2, diagnosisResult.size()),
+				() -> Assertions.assertIterableEquals(resultExpected, diagnosisResult));
 	}
 
 	@Test
 	public void notMultipleOf3and5Test() {
-		fail("Not yet implemented");
+		final int testValue = 11;
+
+		final List<String> resultExpected = Collections.emptyList();
+
+		final List<String> diagnosisResult = healthDiagnosis.redirectPatiens(testValue);
+
+		assertAll("Multiple of 3 and 5 test", () -> assertEquals(2, diagnosisResult.size()),
+				() -> Assertions.assertIterableEquals(resultExpected, diagnosisResult));
 	}
 }
